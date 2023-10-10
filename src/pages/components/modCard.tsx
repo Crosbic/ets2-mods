916304499e29ce469f1894daa1671d46b7a8fa5b
Maxim <contact@crosbic.ru>
2023-10-10 16:24:30 +0300

    Карточки на главной + адаптивка

--- src/pages/components/modCard.tsx
+++ src/pages/components/modCard.tsx
@@ -1,36 +1,68 @@
 import { Card, CardContent, CardMedia, Typography } from '@mui/material'
 
+import styles from './modCard.module.css'
+
 interface ICardData {
   name: string
   description: string
+  update: string
   // image: string
 }
 
 const ModCard = () => {
   const cardData: ICardData[] = [
-    { name: 'amogus', description: 'dvwvsdvsafevsdv' },
-    { name: 'amogus', description: 'dvwvsdvsafevsdv' },
-    { name: 'amogus', description: 'dvwvsdvsafevsdv' },
-    { name: 'amogus', description: 'dvwvsdvsafevsdv' },
+    {
+      name: 'amogus',
+      description: '- Добавлен Jeep Grand Cherokee ’14.',
+      update:
+        'Список машин\n' +
+        '- Mercedes-Benz A(W169, W176), B(W246), C(W202, W203, W204), E(W124, W210, W212, W213, S212), S(W220, W221, C217), R, ML, G, GL, CL, CLK, CLA, CLS(W219, C257), GLA, GLC, GLE, EQC, X, Vario,',
+    },
+    {
+      name: 'amogus',
+      description: '- Добавлен Jeep Grand Cherokee ’14.',
+      update:
+        'Список машин\n' +
+        '- Mercedes-Benz A(W169, W176), B(W246), C(W202, W203, W204), E(W124, W210, W212, W213, S212), S(W220, W221, C217), R, ML, G, GL, CL, CLK, CLA, CLS(W219, C257), GLA, GLC, GLE, EQC, X, Vario,',
+    },
+    {
+      name: 'amogus',
+      description: '- Добавлен Jeep Grand Cherokee ’14.',
+      update:
+        'Список машин\n' +
+        '- Mercedes-Benz A(W169, W176), B(W246), C(W202, W203, W204), E(W124, W210, W212, W213, S212), S(W220, W221, C217), R, ML, G, GL, CL, CLK, CLA, CLS(W219, C257), GLA, GLC, GLE, EQC, X, Vario,',
+    },
+    {
+      name: 'amogus',
+      description: '- Добавлен Jeep Grand Cherokee ’14.',
+      update:
+        'Список машин\n' +
+        '- Mercedes-Benz A(W169, W176), B(W246), C(W202, W203, W204), E(W124, W210, W212, W213, S212), S(W220, W221, C217), R, ML, G, GL, CL, CLK, CLA, CLS(W219, C257), GLA, GLC, GLE, EQC, X, Vario,',
+    },
   ]
 
   return (
     <>
       {cardData.map((data) => {
         return (
-          <div key={data.name}>
-            <Card
-              sx={{ minWidth: '100vh', display: 'flex', flexDirection: 'row' }}
-            >
-              <CardMedia component="img" height="200" image="/pipiska.png" />
-              <CardContent sx={{ minWidth: '100vh' }}>
-                <Typography color="primary" variant="h5">
-                  {data.name}
-                </Typography>
-                <Typography>{data.description}</Typography>
-              </CardContent>
-            </Card>
-          </div>
+          <Card className={styles.card} key={data.name}>
+            <CardMedia
+              component="img"
+              image="/pipiska.png"
+              className={styles.cardMedia}
+            />
+            <CardContent>
+              <Typography color="primary" className={styles.cardHeader}>
+                {data.name}
+              </Typography>
+              <Typography className={styles.cardDescription}>
+                {data.description}
+              </Typography>
+              <Typography className={styles.cardUpdate}>
+                {data.update}
+              </Typography>
+            </CardContent>
+          </Card>
         )
       })}
     </>
